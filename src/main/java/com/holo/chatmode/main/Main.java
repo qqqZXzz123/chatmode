3770176152a76028959409e71749f5013c01e259
Neirno <Gleb.Tsukanov.Gleb@mail.ru>
2020-08-30 00:18:07 +0300

    aa

--- src/main/java/com/holo/chatmode/main/Main.java
+++ src/main/java/com/holo/chatmode/main/Main.java
@@ -1,8 +1,12 @@
 package com.holo.chatmode.main;
 
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.Mod.Instance;
 import net.minecraftforge.fml.common.SidedProxy;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
 import com.holo.chatmode.proxy.CommonProxy;
 import com.holo.chatmode.reference.Reference;
@@ -17,4 +21,19 @@ public class Main {
 	
 	@SidedProxy(clientSide = Reference.CLIENT, serverSide = Reference.COMMON)
 	public static CommonProxy proxy;
+	
+	@EventHandler
+    public void preInit(final FMLPreInitializationEvent event) {
+        Main.proxy.preInit(event);
+    }
+    
+    @EventHandler
+    public void init(final FMLInitializationEvent event) {
+        Main.proxy.init(event);
+    }
+    
+    @EventHandler
+    public void postInit(final FMLPostInitializationEvent event) {
+        Main.proxy.postInit(event);
+    }
 }
